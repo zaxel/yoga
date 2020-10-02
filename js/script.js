8cc2176d4d5edefceddcb87d4d6ebbc27c4d6aa8
zaxel <sae11.z.alex@gmail.com>
2020-10-02 13:09:46 +0100

    yoga landing page

---
+++ js/script.js
@@ -0,0 +1,33 @@
+jQuery(function(){
+    // Inline popups
+    $('#inline-popups').magnificPopup({
+        delegate: 'a',
+        removalDelay: 500, //delay removal by X to allow out-animation
+        callbacks: {
+        beforeOpen: function() {
+            this.st.mainClass = this.st.el.attr('data-effect');
+        }
+        },
+        midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+    });
+});
+  
+$(document).ready(function(){
+    // Optimalisation: Store the references outside the event handler:
+    let $window = $(window);
+
+    function checkWidth() {
+        let windowsize = $window.width();
+        if (windowsize < 688) {
+            //if the window is less than 688px wide then turn on jScrollPane..
+            $( "#accordion" ).accordion();
+        } else{
+            $( "#accordion" ).accordion("destroy");
+        }
+    }
+    // Execute on load
+    checkWidth();
+    // Bind event listener
+    $(window).resize(checkWidth);
+    
+});
